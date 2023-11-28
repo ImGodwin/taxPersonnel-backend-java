0a424d1eb766e40568dc8755316be4734e6256de
Godwin <godwinmail6@gmail.com>
2023-11-28 19:16:13 +0100

    Tax Personnel service created

--- src/main/java/Godwin/taxSolution/service/TaxPersonnelService.java
+++ src/main/java/Godwin/taxSolution/service/TaxPersonnelService.java
@@ -1,6 +1,8 @@
 package Godwin.taxSolution.service;
 
 import Godwin.taxSolution.entities.TaxPersonnel;
+import Godwin.taxSolution.exceptions.NotFoundException;
+import Godwin.taxSolution.payloads.TaxPeronnelDTO;
 import Godwin.taxSolution.repository.TaxPersonneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -9,11 +11,17 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class TaxPersonnelService {
 
+    //need to add image with cloudinary
     @Autowired
     TaxPersonneRepository taxPersonneRepository;
+
+    @Autowired
+    private CityService cityService;
 
     public Page<TaxPersonnel> getTaxPersonnels(int page, int size, String orderBy){
 
@@ -21,5 +29,30 @@ public class TaxPersonnelService {
         return taxPersonneRepository.findAll(pageable);
     }
 
+    public TaxPersonnel findById(UUID id) throws NotFoundException {
+
+        return taxPersonneRepository.findById(id).orElseThrow(() -> new NotFoundException(id));
+    }
+
+    public TaxPersonnel findByIdAndUpdate(UUID id, TaxPeronnelDTO body)
+    {
+        TaxPersonnel taxPersonnel = this.findById(id);
+
+        taxPersonnel.setName(body.name());
+        taxPersonnel.setSurname(body.surname());
+        taxPersonnel.setEmail(body.email());
+        taxPersonnel.setTelephone(body.telephone());
+        taxPersonnel.setAddress(body.address());
+        taxPersonnel.setCityName(body.cityName());
+        taxPersonnel.setPIva(body.pIva());
+        taxPersonnel.setDescription(body.description());
+        taxPersonnel.setImage("http://ui-avatars.com/api/?name="+body.name() + "+" + body.surname());
+        return taxPersonneRepository.save(taxPersonnel);
+    }
+
+    public void findDeviceByIdAndDelete(UUID id) throws NotFoundException {
+        TaxPersonnel foundDevice = this.findById(id);
+        taxPersonneRepository.delete(foundDevice);
+    }
 
 }
