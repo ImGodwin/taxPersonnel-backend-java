eecfb90ca3578422612cb732a013a79ac2f729b5
Godwin <godwinmail6@gmail.com>
2023-11-28 19:00:58 +0100

    Get all method added in tax personnel service class

--- src/main/java/Godwin/taxSolution/service/TaxPersonnelService.java
+++ src/main/java/Godwin/taxSolution/service/TaxPersonnelService.java
@@ -1,4 +1,25 @@
 package Godwin.taxSolution.service;
 
+import Godwin.taxSolution.entities.TaxPersonnel;
+import Godwin.taxSolution.repository.TaxPersonneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TaxPersonnelService {
+
+    @Autowired
+    TaxPersonneRepository taxPersonneRepository;
+
+    public Page<TaxPersonnel> getTaxPersonnels(int page, int size, String orderBy){
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(orderBy));
+        return taxPersonneRepository.findAll(pageable);
+    }
+
+
 }
